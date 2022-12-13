6f0545d33bb987dd3d8187c6c454425fba547afc
GeraldineGomez <46997659+GeraldineGomez@users.noreply.github.com>
2022-12-13 11:47:14 -0500

    adding rt calculation, results and graphs

--- rt/rt.R
+++ rt/rt.R
@@ -4,30 +4,17 @@ library(EpiEstim)
 library(dplyr)
 library(tidyverse)
 library(readxl)
+library(lubridate)
 
-# paths
-DATA_PATH = 'data/'
-OUT_PATH = 'rt/outputs'
-
-# read data
-df_linelist <- read_excel(paste0(DATA_PATH, 'Linelist_grp4.xlsx'))
-
-# compute incidence
-df_incidence <- df_linelist %>% 
-  count(onset_date, sort=TRUE, name='I')
-
-####Completando las fechas faltantes####
 complete_dates <- function(df){
-  all_dates <- data.frame(dates = seq.Date(min(df$onset_date), 
-                                           max(df$onset_date), 
+  all_dates <- data.frame(dates = seq.Date(min(df$dates), 
+                                           max(df$dates), 
                                            by = 'day')
-                          )  
+  )  
   df <- left_join(all_dates, df)
   df$I[is.na(df$I)] <- 0
   return(df)
 } 
-
-df_incidence <- df_incidence %>% complete_dates()
 
 compute_rt <- function(df,
                        rt_days=14,
@@ -39,18 +26,41 @@ compute_rt <- function(df,
   rt_data  <- estimate_R(df, method = "parametric_si",
                          config = make_config(list(
                            mean_si = mean_si, std_si = std_si,
-                           t_start= t_start, t_end = t_end )))
-  
-  df_rt <- rt_data$R
-  df_rt$window_start <- min(df$dates) + df_rt$t_start
-  df_rt$window_end <- min(df$dates) + df_rt$t_end
-  df_rt <- df_rt %>% filter(window_end <= '2022-12-31')
-  return(df_rt)
+                           t_start = t_start, t_end = t_end )))
+  return(rt_data)
 }
 
+# paths
+DATA_PATH = 'data/'
+OUT_PATH = 'outputs/'
+
+# read data
+df_linelist <- readxl::read_excel(paste0(DATA_PATH, 'Linelist_grp4.xlsx'))
+
+# compute incidence
+df_incidence <- df_linelist %>% 
+  count(.data$onset_date, sort = TRUE, name = 'I')
+
+# format incidence
+df_incidence$onset_date <- as.Date(df_incidence$onset_date, format = "%m/%d/%y")
+
+# clean incidence
+df_incidence <- df_incidence[-1, ]
+df_incidence$I[is.na(df_incidence$I)] <- 0
+
+# rename column
+names(df_incidence)[1] <- 'dates'
+
+# complete dates
+df_incidence <- df_incidence %>% complete_dates()
+
+# compute rt
 df_rt <- df_incidence %>% compute_rt()
-df_rt_60 <- df_incidence_60 %>% compute_rt() 
 
 # save data
 write_csv(df_rt, paste0(OUT_PATH, 'rt_all_ages.csv'))
-write_csv(df_rt_60, paste0(OUT_PATH, 'rt_60_plus.csv'))
+
+# plotting data
+png(file = paste0(OUT_PATH, "graphs_rt.png"))
+plot(df_rt, legend = FALSE)
+dev.off()
